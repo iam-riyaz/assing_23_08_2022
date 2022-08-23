53ce4d7aa80127b80c14910900c453759bb65121
RIYAZ AHMAD <riyazahmad.online@gmail.com>
2022-08-23 14:58:32 +0530

    assignment for noida based company

---
+++ login/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Login } from './components/login'
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <Login></Login>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ login/src/components/Login.jsx
@@ -0,0 +1,55 @@
+import {Button,Modal, Box, TextField} from "@mui/material"
+
+import { useState } from "react";
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+    
+
+
+
+export const Login=()=>{
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    return(
+        <>
+        <Button onClick={handleOpen}>Login</Button>
+<Modal
+  open={open}
+  onClose={handleClose}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+>
+  <Box sx={style}>
+  <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </Box>
+
+  </Box>
+</Modal>
+        </>
+    )
+}
+
+
+
